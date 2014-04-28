31da31de2c5bc6a8eca4c63d2d45b5180dcf66c0
Matthew Furseman <matthew.furseman@diamond.ac.uk>
2014-04-28 14:15:18 +0100

    OpiShell calculates window borders after opening.
    This allows the window size to take into account window borders
    with differing dimensions.

--- applications/plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/runmode/OPIShell.java
+++ applications/plugins/org.csstudio.opibuilder/src/org/csstudio/opibuilder/runmode/OPIShell.java
@@ -7,10 +7,11 @@ import org.csstudio.opibuilder.model.DisplayModel;
 import org.csstudio.opibuilder.persistence.XMLUtil;
 import org.csstudio.opibuilder.util.ResourceUtil;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.GraphicalViewer;
 import org.eclipse.gef.editparts.ScalableFreeformRootEditPart;
 import org.eclipse.gef.ui.parts.GraphicalViewerImpl;
+import org.eclipse.swt.events.ShellEvent;
+import org.eclipse.swt.events.ShellListener;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -42,23 +43,24 @@ public class OPIShell {
         viewer.setContents(displayModel);
         displayModel.setViewer(viewer);
 
-        shell.setText(path.toString());
-       
+        shell.setText(path.toString()); // Set title
+        // Resize the shell after it's open, so we can take into account different window borders
+        shell.addShellListener(new ShellListener() {
+			public void shellIconified(ShellEvent e) {}
+			public void shellDeiconified(ShellEvent e) {}
+			public void shellDeactivated(ShellEvent e) {}
+			public void shellClosed(ShellEvent e) {}
+			public void shellActivated(ShellEvent e) {
+				int frameX = shell.getSize().x - shell.getClientArea().width;
+				int frameY = shell.getSize().y - shell.getClientArea().height;
+				shell.setSize(displayModel.getSize().width + frameX, displayModel.getSize().height + frameY);
+			}
+		});
         shell.pack();
         shell.open();
-        
-        Rectangle windowBounds = getWindowBounds();
-		shell.setSize(windowBounds.width + 18,
-				windowBounds.height + 38);
     }
     
     public Shell getShell() {
     	return shell;
-    }
-    
-    private Rectangle getWindowBounds() {
-		Rectangle windowBounds = new Rectangle(displayModel.getLocation(),
-				displayModel.getSize());
-		return windowBounds;
     }
 }
