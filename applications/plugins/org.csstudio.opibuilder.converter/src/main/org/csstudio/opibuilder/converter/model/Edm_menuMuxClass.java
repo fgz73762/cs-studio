ba1381bb4ebb54194194dd0543645c7eafefcf2e
Nick Battam <nicholas.battam@diamond.ac.uk>
2014-05-09 15:05:24 +0100

    CSS-24 Implement MuxMenu EDM control

---
+++ applications/plugins/org.csstudio.opibuilder.converter/src/main/org/csstudio/opibuilder/converter/model/Edm_menuMuxClass.java
@@ -0,0 +1,28 @@
+package org.csstudio.opibuilder.converter.model;
+
+/**EDM Menu Mux widget
+ * @author Xihui Chen
+ *
+ */
+public class Edm_menuMuxClass extends EdmWidget {
+
+	@EdmAttributeAn private int numItems;
+	@EdmAttributeAn @EdmOptionalAn private EdmMultiStrings symbolTag;
+	@EdmAttributeAn @EdmOptionalAn private EdmMultiStrings symbol0;
+
+	public Edm_menuMuxClass(EdmEntity genericEntity) throws EdmException {
+		super(genericEntity);
+	}
+
+	public int getNumItems() {
+		return numItems;
+	}
+
+	public EdmMultiStrings getSymbolTags() {
+		return symbolTag;
+	}
+
+	public EdmMultiStrings getSymbolZero() {
+		return symbol0;
+	}
+}
